af5042c422022f8d484b877c2546b0cb0a76711e
齋藤将平 <shohei@shohei-MacBook.local>
2025-08-28 21:57:47 +0900

    再コミット

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import ui
 import json
 
+class TskData:
+    def __init__(self,tsk,day,juge):
+        self.tsk=tsk
+        self.day=day
+        self.juge=juge
+
+#main処理
+def main_function(tskdata):
+    write_json(tskdata.tsk,tskdata.day,tskdata.juge)
+
+
 #ファイルの読み込み
-with open("data.txt", mode="w") as json_file:
-    json_file.write("こんにちは\n")
-    json_file.write("私は人間ですか?")
+def write_json(a,b,c):
+    with open("data.json", mode="w") as json_file:
+        json_file.writelines(f"{a},{b},{c}")
--- ui.py
+++ ui.py
@@ -1,4 +1,15 @@
 import tkinter as ui
+from main import main_function
+from main import TskData
+
+#入力内容を読み出し処理
+def get_values():
+    values_tsk=str(input_tsk.get())
+    values_day=input_day.get()
+    values_juge=0       #タスク未実行の状態
+    tsk_management=TskData(values_tsk,values_day,values_juge)
+    main_function(tsk_management)
+
 
 root=ui.Tk()
 root.title(u"ToDoリスト")
@@ -10,13 +21,13 @@ label_tsk.grid(row=0,column=0)
 label_day=ui.Label(root,text="日付")
 label_day.grid(row=1,column=0)
 
-entry_tsk=ui.Entry(root,width=20)
+input_tsk=entry_tsk=ui.Entry(root,width=20)
 entry_tsk.grid(row=0,column=1)
 
-entry_day=ui.Entry(root,width=20)
+input_day=entry_day=ui.Entry(root,width=20)
 entry_day.grid(row=1,column=1)
 
-button_add=ui.Button(root,text="タスクの追加")
+button_add=ui.Button(root,text="タスクの追加",command=get_values)
 button_add.grid(row=3,column=0)
 
 button_comp=ui.Button(root,text="タスクの完了")
@@ -24,7 +35,5 @@ button_comp.grid(row=4,column=0)
 
 button_del=ui.Button(root,text="タスクの削除")
 button_del.grid(row=5,column=0)
-
-
 
 root.mainloop()
