80be2844db2c1c585d735e0f74d325f3b792a78c
齋藤将平 <shohei@shohei-MacBook.local>
2025-08-28 21:50:08 +0900

    データを入力できるようにした

--- ui.py
+++ ui.py
@@ -4,4 +4,27 @@ root=ui.Tk()
 root.title(u"ToDoリスト")
 root.geometry("500x500")
 
+label_tsk=ui.Label(root,text="タスク")
+label_tsk.grid(row=0,column=0)
+
+label_day=ui.Label(root,text="日付")
+label_day.grid(row=1,column=0)
+
+entry_tsk=ui.Entry(root,width=20)
+entry_tsk.grid(row=0,column=1)
+
+entry_day=ui.Entry(root,width=20)
+entry_day.grid(row=1,column=1)
+
+button_add=ui.Button(root,text="タスクの追加")
+button_add.grid(row=3,column=0)
+
+button_comp=ui.Button(root,text="タスクの完了")
+button_comp.grid(row=4,column=0)
+
+button_del=ui.Button(root,text="タスクの削除")
+button_del.grid(row=5,column=0)
+
+
+
 root.mainloop()
