6d365b7339dd590ed8aec5cfcb46fee1844176a2
齋藤将平 <shohei@shohei-MacBook.local>
2025-08-29 23:29:12 +0900

    2回目_UI変更_追加を押しても更新する

--- ui.py
+++ ui.py
@@ -12,32 +12,27 @@ def get_values():
     tsk_management=[]
     tsk_management=TskData(values_tsk,values_day,values_juge)
     main_function(tsk_management)
+    i=7 #タスク進行の最終行
+    read_load(i)
+
+def read_load(a):
+    with open("data.csv",mode="r") as read_csv:
+        dataread=csv.reader(read_csv)   #csvファイルの読み出し
+        rows=list(dataread)     #csvファイル内のデータをリスト化
+        #出力s
+        for s in rows:
+            #完了列に1があったら除外する処理はここに追加
+            for j in range(3):
+                label_tsk_csv=ui.Label(root,text=s[j])
+                label_tsk_csv.grid(row=a,column=j)
+            a+=1
+    return a
+
 
 root=ui.Tk()
 root.title(u"ToDoリスト")
 root.geometry("500x500")
-
-#進行中タスク表示ラベル
 i=0
-label_tsk_now=ui.Label(root,text="進行中タスク")
-label_tsk_now.grid(row=i,column=0)
-label_tsk_now=ui.Label(root,text="期日")
-label_tsk_now.grid(row=i,column=1)
-label_tsk_now=ui.Label(root,text="完了")
-label_tsk_now.grid(row=i,column=2)
-i+=1
-
-#進行中タスク表示
-with open("data.csv",mode="r") as read_csv:
-    dataread=csv.reader(read_csv)   #csvファイルの読み出し
-    rows=list(dataread)     #csvファイル内のデータをリスト化
-    #出力s
-    for s in rows:
-        #完了列に1があったら除外する処理はここに追加
-        for j in range(3):
-            label_tsk_csv=ui.Label(root,text=s[j])
-            label_tsk_csv.grid(row=i,column=j)
-        i+=1
 
 #タスク追加入力フォーム
 label_tsk=ui.Label(root,text="タスク")
@@ -66,6 +61,18 @@ i+=1
 #タスク削除ボタン
 button_del=ui.Button(root,text="タスクの削除")
 button_del.grid(row=i+1,column=0)
+i+=2
+
+#進行中タスク表示ラベル
+label_tsk_now=ui.Label(root,text="進行中タスク")
+label_tsk_now.grid(row=i,column=0)
+label_tsk_now=ui.Label(root,text="期日")
+label_tsk_now.grid(row=i,column=1)
+label_tsk_now=ui.Label(root,text="完了")
+label_tsk_now.grid(row=i,column=2)
 i+=1
+
+#進行中タスク表示
+i=read_load(i)
 
 root.mainloop()
